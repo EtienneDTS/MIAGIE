074974889c43ab3e12ff326f85866003abc575ee
EtienneDTS <detoisienetienne@gmail.com>
2024-10-07 11:31:01 +0200

    first commit

---
+++ src/main/java/Serdaigle/MIAGIE/MiagieApplication.java
@@ -0,0 +1,13 @@
+package Serdaigle.MIAGIE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiagieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiagieApplication.class, args);
+	}
+
+}
